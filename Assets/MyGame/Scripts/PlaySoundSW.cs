080873b6fde281b4099af54f3b988305fd730cfd
Nico <nicolas.bachleitner03@htl-salzburg.ac.at>
2021-05-10 10:26:32 +0200

    add soundbutton (schoolwork)

---
+++ Assets/MyGame/Scripts/PlaySoundSW.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundSW : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSourceCam;
+    public void PlaySound()
+    {
+        audioSourceCam.Play();
+        StartCoroutine("PlaySoundForThreeSeconds");
+    }
+    IEnumerator PlaySoundForThreeSeconds()
+    {
+        yield return new WaitForSeconds(3);
+        audioSourceCam.Stop();
+    }
+}
