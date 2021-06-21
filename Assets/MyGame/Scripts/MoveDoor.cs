5cd3d357cf1257d2760f69a51559bc45188b4941
Nico <nicolas.bachleitner03@htl-salzburg.ac.at>
2021-06-21 07:53:16 +0200

    Update Animation Door

--- Assets/MyGame/Scripts/MoveDoor.cs
+++ Assets/MyGame/Scripts/MoveDoor.cs
@@ -7,11 +7,17 @@ public class MoveDoor : MonoBehaviour
     float t;
     bool moveDownB = false;
     public bool openDoor;
+    bool stateChanged;
     void Update()
     {
         if (transform.position.y < 5f && !moveDownB && openDoor)
         {
             MoveUp();
+            if (stateChanged == false)
+            {
+                GetComponent<AudioSource>().Play();
+                stateChanged = true;
+            }
         }
         if (transform.position.y > 1.7f && moveDownB)
         {
@@ -22,7 +28,6 @@ public class MoveDoor : MonoBehaviour
     {
         transform.position += new Vector3(0, Mathf.Lerp(0, 0.4f, t), 0);
         t += 0.04f * Time.deltaTime;
-        GetComponent<AudioSource>().Play();
     }
     void MoveDown()
     {
